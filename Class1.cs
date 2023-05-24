efe636c4e6dbc4942b044fc3fa35d5b5ce5e0f1f
Mliybs <2491988463@qq.com>
2023-05-24 20:34:46 +0800

    initialize

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace MliybsLibrary
+{
+    public class MliybsAttribute : Attribute
+    {
+        public MliybsAttribute(MliybsInfo info) => Info = info;
+
+        public MliybsInfo Info { get; set; }
+    }
+
+    public enum MliybsInfo
+    {
+        OnUse = 0b1
+    }
+
+    public static class MliybsLibraryStatic
+    {
+        public static MliybsInfo? GetMliybsInfo(this object self) => self.GetType().GetCustomAttribute<MliybsAttribute>()!.Info;
+
+        public static bool TryGetMliybsInfo(this object self, out MliybsInfo? info)
+        {
+            var attr = self.GetType().GetCustomAttribute<MliybsAttribute>();
+
+            if (attr == null)
+            {
+                info = null;
+
+                return false;
+            }
+
+            else
+            {
+                info = attr.Info;
+
+                return true;
+            }
+        }
+
+        public static bool IsMliybsInfoExisting(this object self)
+        {
+            if (self.GetType().GetCustomAttribute<MliybsAttribute>() == null)
+                return false;
+
+            else
+                return true;
+        }
+    }
+}
